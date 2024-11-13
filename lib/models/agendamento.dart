47188be5406aca2ee3c5d2797542b3854b2e4dd1
MrBigBotnx <daiwenxuam78@gmail.com>
2024-11-13 08:21:43 +0200

    Passando as alterações feitas em master para o backend

--- lib/models/agendamento.dart
+++ lib/models/agendamento.dart
@@ -1,24 +1,10 @@
 class Agendamento {
-  final String id;
-  final String clienteId;
-  final String servicoId;
-  final DateTime data;
-
   Agendamento({
     required this.id, 
     required this.clienteId,
     required this.servicoId, 
     required this.data
   });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'clienteId': clienteId,
-      'servicoId': servicoId,
-      'data': data.toIso8601String(),
-    };
-  }
 
   factory Agendamento.fromMap(Map<String, dynamic> map) {
     return Agendamento(
@@ -27,5 +13,19 @@ class Agendamento {
       servicoId: map['servicoId'] ?? '',
       data: DateTime.parse(map['data']),
     );
+  }
+
+  final String clienteId;
+  final DateTime data;
+  final String id;
+  final String servicoId;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'clienteId': clienteId,
+      'servicoId': servicoId,
+      'data': data.toIso8601String(),
+    };
   }
 }
