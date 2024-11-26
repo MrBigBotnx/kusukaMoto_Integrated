9b5fe1a2e17890e21147c700e5782a79a1e0eafe
MrBigBotnx <daiwenxuam78@gmail.com>
2024-11-27 00:16:07 +0200

    mmmmm

--- lib/screens/admin_edit_service.dart
+++ lib/screens/admin_edit_service.dart
@@ -1,16 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:kusuka_moto/models/categoria.dart';
 import 'package:kusuka_moto/models/servico.dart';
 import 'package:kusuka_moto/services/database_service.dart';
 
 class AdminEditServicePage extends StatefulWidget {
   final String serviceId;
 
-  const AdminEditServicePage(
-      {super.key, required this.serviceId, required Servico servico});
+  const AdminEditServicePage({Key? key, required this.serviceId})
+      : super(key: key);
 
   @override
   _AdminEditServicePageState createState() => _AdminEditServicePageState();
@@ -20,6 +20,7 @@ class _AdminEditServicePageState extends State<AdminEditServicePage> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
+  CategoriaServico? categoriaSelecionada;
   bool availability = true;
   File? _selectedIcon;
   final DatabaseService _databaseService = DatabaseService();
@@ -40,9 +41,67 @@ class _AdminEditServicePageState extends State<AdminEditServicePage> {
         descriptionController.text = service.descricao;
         priceController.text = service.preco.toString();
         availability = service.disponibilidade;
-        _selectedIcon = service.iconBase64 as File?;
+        categoriaSelecionada = service.categoria;
       });
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Editar Serviço'),
+        backgroundColor: const Color(0xFF070245),
+      ),
+      backgroundColor: const Color(0xFF00E0C6),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildIconPicker(),
+              _buildTextField('Nome do Serviço', Icons.work, nameController),
+              const SizedBox(height: 10),
+              _buildTextField('Descrição', Icons.description, descriptionController),
+              const SizedBox(height: 10),
+              _buildTextField(
+                'Preço',
+                Icons.attach_money,
+                priceController,
+                isNumeric: true,
+              ),
+              const SizedBox(height: 10),
+              _buildDropdownCategoria(),
+              const SizedBox(height: 10),
+              _buildAvailabilityCheckbox(),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: _saveService,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF070245),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 80, vertical: 15),
+                  ),
+                  child: const Text(
+                    'SALVAR',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _buildIconPicker() {
@@ -58,23 +117,24 @@ class _AdminEditServicePageState extends State<AdminEditServicePage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Ícone',
             style: TextStyle(
               fontSize: 16,
               color: Color(0xFF070245),
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Container(
             height: 100,
             width: 100,
             decoration: BoxDecoration(
-              border: Border.all(color: Color(0xFF070245)),
+              border: Border.all(color: const Color(0xFF070245)),
               borderRadius: BorderRadius.circular(10),
             ),
             child: _selectedIcon == null
-                ? Icon(Icons.image, color: Color(0xFF070245), size: 50)
+                ? const Icon(Icons.image,
+                    color: Color(0xFF070245), size: 50)
                 : Image.file(_selectedIcon!, fit: BoxFit.cover),
           ),
         ],
@@ -82,13 +142,85 @@ class _AdminEditServicePageState extends State<AdminEditServicePage> {
     );
   }
 
+  Widget _buildTextField(
+      String label, IconData icon, TextEditingController controller,
+      {bool isNumeric = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 16,
+            color: Color(0xFF070245),
+          ),
+        ),
+        TextField(
+          controller: controller,
+          keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
+          decoration: InputDecoration(
+            prefixIcon: Icon(icon, color: const Color(0xFF070245)),
+            hintText: 'Digite o $label',
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFF070245)),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Color(0xFF070245)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDropdownCategoria() {
+    return DropdownButtonFormField<CategoriaServico>(
+      value: categoriaSelecionada,
+      decoration: const InputDecoration(
+        labelText: 'Categoria',
+        border: OutlineInputBorder(),
+      ),
+      items: CategoriaServico.values.map((categoria) {
+        return DropdownMenuItem(
+          value: categoria,
+          child: Text(categoria.descricao),
+        );
+      }).toList(),
+      onChanged: (value) {
+        setState(() {
+          categoriaSelecionada = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildAvailabilityCheckbox() {
+    return Row(
+      children: [
+        Checkbox(
+          value: availability,
+          onChanged: (value) {
+            setState(() {
+              availability = value!;
+            });
+          },
+        ),
+        const Text(
+          'Disponível',
+          style: TextStyle(fontSize: 16, color: Color(0xFF070245)),
+        ),
+      ],
+    );
+  }
+
   void _saveService() async {
     try {
       if (nameController.text.isEmpty ||
           descriptionController.text.isEmpty ||
-          priceController.text.isEmpty) {
+          priceController.text.isEmpty ||
+          categoriaSelecionada == null) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Por favor, preencha todos os campos.')),
+          const SnackBar(content: Text('Por favor, preencha todos os campos.')),
         );
         return;
       }
@@ -106,12 +238,13 @@ class _AdminEditServicePageState extends State<AdminEditServicePage> {
         preco: double.parse(priceController.text),
         disponibilidade: availability,
         iconBase64: iconBase64,
+        categoria: categoriaSelecionada!,
       );
 
       await _databaseService.updateService(updatedService);
 
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Serviço atualizado com sucesso!')),
+        const SnackBar(content: Text('Serviço atualizado com sucesso!')),
       );
 
       Navigator.pop(context);
@@ -120,150 +253,5 @@ class _AdminEditServicePageState extends State<AdminEditServicePage> {
         SnackBar(content: Text('Erro ao atualizar o serviço: $e')),
       );
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Editar Serviço',
-          style: TextStyle(color: const Color.fromARGB(255, 236, 235, 235)),
-        ),
-        backgroundColor: Color(0xFF070245),
-      ),
-      backgroundColor: Color(0xFF00E0C6),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildIconPicker(),
-              _buildTextField('Nome do Serviço', Icons.work, nameController),
-              SizedBox(height: 10),
-              _buildMarkdownEditor(),
-              SizedBox(height: 10),
-              _buildTextField(
-                'Preço',
-                Icons.attach_money,
-                priceController,
-                isNumeric: true,
-              ),
-              SizedBox(height: 10),
-              _buildAvailabilityCheckbox(),
-              SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: _saveService,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF070245),
-                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                  ),
-                  child: Text(
-                    'SALVAR',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTextField(
-      String label, IconData icon, TextEditingController controller,
-      {bool isNumeric = false}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 16,
-            color: Color(0xFF070245),
-          ),
-        ),
-        TextField(
-          controller: controller,
-          keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
-          decoration: InputDecoration(
-            prefixIcon: Icon(icon, color: Color(0xFF070245)),
-            hintText: 'Digite o $label',
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Color(0xFF070245)),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Color(0xFF070245)),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildMarkdownEditor() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Descrição',
-          style: TextStyle(fontSize: 16, color: Color(0xFF070245)),
-        ),
-        SizedBox(height: 5),
-        Container(
-          height: 150,
-          decoration: BoxDecoration(
-            border: Border.all(color: Color(0xFF070245)),
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: TextField(
-            controller: descriptionController,
-            maxLines: null,
-            keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
-              hintText: 'Digite a descrição (suporta Markdown)',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(10),
-            ),
-          ),
-        ),
-        SizedBox(height: 10),
-        MarkdownBody(
-          data: descriptionController.text,
-          styleSheet: MarkdownStyleSheet(
-            p: TextStyle(fontSize: 14),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildAvailabilityCheckbox() {
-    return Row(
-      children: [
-        Checkbox(
-          value: availability,
-          onChanged: (value) {
-            setState(() {
-              availability = value!;
-            });
-          },
-        ),
-        Text(
-          'Disponível',
-          style: TextStyle(fontSize: 16, color: Color(0xFF070245)),
-        ),
-      ],
-    );
   }
 }
