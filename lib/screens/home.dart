c771270ad0445d34236ac59c8c631de5dccccc89
MrBigBotnx <daiwenxuam78@gmail.com>
2024-11-28 23:08:39 +0200

    Changing

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -224,14 +224,20 @@ class _HomePageState extends State<HomePage> {
               ),
               Column(
                 children: [
-                  Text(
-                    'Categorias',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Categorias',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
+                  SizedBox(
+                    height: 15,
+                  )
                 ],
               ),
               Text(
@@ -327,60 +333,60 @@ class _HomePageState extends State<HomePage> {
 
   Column _seachField() {
     return Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-                  decoration: BoxDecoration(boxShadow: [
-                    BoxShadow(
-                        color: Color(0xff1D1617).withOpacity(0.11),
-                        blurRadius: 40,
-                        spreadRadius: 0.0),
-                  ]),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: EdgeInsets.all(15),
-                      hintText: 'Pesquisa',
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: SvgPicture.asset('assets/icons/search.svg'),
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+                color: Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0),
+          ]),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Pesquisa',
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(10),
+                child: SvgPicture.asset('assets/icons/search.svg'),
+              ),
+              suffixIcon: SizedBox(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
                       ),
-                      suffixIcon: SizedBox(
-                        width: 100,
-                        child: IntrinsicHeight(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              VerticalDivider(
-                                color: Colors.black,
-                                indent: 10,
-                                endIndent: 10,
-                                thickness: 0.1,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 1.0,
-                                  top: 8.0,
-                                  right: 15.0,
-                                  bottom: 8.0,
-                                ),
-                                child: SvgPicture.asset(
-                                  'assets/icons/filter.svg',
-                                ),
-                              ),
-                            ],
-                          ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 1.0,
+                          top: 8.0,
+                          right: 15.0,
+                          bottom: 8.0,
+                        ),
+                        child: SvgPicture.asset(
+                          'assets/icons/filter.svg',
                         ),
                       ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide.none),
-                    ),
+                    ],
                   ),
-                )
-              ],
-            );
+                ),
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
 
